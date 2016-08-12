5deedd2cf4045a717224b7ec339ac0dcfdf89021
Inoki <veyx.shaw@gmail.com>
2016-08-12 21:40:36 +0000

    Init Page With AD

--- app/src/main/java/com/veyxstudio/shulehu/InitActivity.java
+++ app/src/main/java/com/veyxstudio/shulehu/InitActivity.java
@@ -7,36 +7,68 @@ import android.content.SharedPreferences;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Window;
+import android.view.WindowManager;
 
 import com.veyxstudio.shulehu.util.AMarkDataBaseHelper;
 import com.veyxstudio.shulehu.util.KeyWordHelper;
+
+import cn.domob.android.ads.RTSplashAd;
+import cn.domob.android.ads.RTSplashAdListener;
+import cn.domob.android.ads.SplashAd;
 
 /**
  * Created by Veyx Shaw on 16-1-27.
  * Init files and states.
  */
-public class InitActivity extends Activity implements Runnable{
+public class InitActivity extends Activity{
     private static final String LOG_TAG = "InitActivity";
+
+    RTSplashAd rtSplashAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_init);
 
+        rtSplashAd = new RTSplashAd(this, KeyWordHelper.PublishID, KeyWordHelper.StartID,
+                SplashAd.SplashMode.SplashModeFullScreen);
+        rtSplashAd.setRTSplashAdListener(new RTSplashAdListener() {
+            @Override
+            public void onRTSplashDismiss() {
+                Log.i("DomobSDKDemo", "onRTSplashClosed");
+                jump();
+                rtSplashAd.closeRTSplash();
+            }
+
+            @Override
+            public void onRTSplashLoadFailed() {
+                Log.i("DomobSDKDemo", "onRTSplashLoadFailed");
+            }
+
+            @Override
+            public void onRTSplashPresent() {
+                Log.i("DomobSDKDemo", "onRTSplashStart");
+            }
+
+        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                rtSplashAd.splash(InitActivity.this, InitActivity.this.findViewById(R.id.init_splash));
+            }
+        }, 1);
+
         // Run new thread.
-        new Thread(this).start();
+        // new Thread(this).start();
     }
 
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void jump() {
         // Check first login
         SharedPreferences use =
                 getSharedPreferences(KeyWordHelper.uFileName,
